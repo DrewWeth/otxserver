4c2bcb862f8ed5b2b904d2d15923593996e5563b
Drew Wetherington <dwetherington@cibotechnologies.com>
2018-01-13 18:13:21 -0600

    clean.lua works now

--- path_10_11/data/talkactions/scripts/clean.lua
+++ path_10_11/data/talkactions/scripts/clean.lua
@@ -7,8 +7,6 @@ function onSay(player, words, param)
 		return false
 	end
 
-	qrswtuv9545872
-
 	local itemCount = cleanMap()
 	if itemCount > 0 then
 		player:sendTextMessage(MESSAGE_STATUS_WARNING, "Cleaned " .. itemCount .. " item" .. (itemCount > 1 and "s" or "") .. " from the map.")
