6f0b19ff166704490b3111cdb37f06555242b773
Drew Wetherington <dwetherington@cibotechnologies.com>
2018-01-13 22:45:52 -0600

    gm mode

--- path_10_11/data/talkactions/scripts/ghost.lua
+++ path_10_11/data/talkactions/scripts/ghost.lua
@@ -1,13 +1,17 @@
 function onSay(cid, words, param)
 	local player = Player(cid)
-	
-	player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("Value: %s", player:hasCondition(CONDITION_INFIGHT)))
-	if ((player:hasCondition(CONDITION_INFIGHT) and not Tile(player:getPosition()):hasFlag(TILESTATE_PROTECTIONZONE)) or isPlayerPzLocked(cid)) then
+	local position = player:getPosition()
+
+
+	if not player:getGroup():getAccess() then
+		return true
+	end
+
+	if ((player:hasCondition(CONDITION_INFIGHT) and not Tile(position):hasFlag(TILESTATE_PROTECTIONZONE)) or isPlayerPzLocked(cid)) then
 		player:sendTextMessage(MESSAGE_INFO_DESCR, "You cannot ghost while in combat.")
 		return false
 	end
 
-	local position = player:getPosition()
 	local isGhost = not player:isInGhostMode()
 
 	player:setGhostMode(isGhost)
---
+++ path_10_11/data/talkactions/scripts/gmmode.lua
@@ -0,0 +1,27 @@
+local gmOutfit = {lookType = 75, lookHead = 10, lookLegs = 10, lookBody = 10, lookFeet = 10, lookAddons = 0}
+local groupIdStorageValue = 17293
+
+function onSay(cid, words, param)
+	local player = Player(cid)
+	local position = player:getPosition()
+
+	if ((player:hasCondition(CONDITION_INFIGHT) and not Tile(position):hasFlag(TILESTATE_PROTECTIONZONE)) or isPlayerPzLocked(cid)) then
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You cannot change to GM mode while in combat.")
+		return false
+	end
+
+	local playerGroupId = getPlayerGroupId(cid)
+	local isGmMode = playerGroupId == 0
+
+	if not isGmMode then
+		setPlayerStorageValue(cid, groupIdStorageValue, playerGroupId)
+		setPlayerGroupId(cid, 0)
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You are infused with the power of GM mode.")
+		position:sendMagicEffect(7)
+	else
+		setPlayerGroupId(cid, getPlayerStorageValue(cid, groupIdStorageValue))
+		player:sendTextMessage(MESSAGE_INFO_DESCR, "You feel normal again.")
+		position:sendMagicEffect(67)
+	end
+	return false
+end
